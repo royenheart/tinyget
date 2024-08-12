ba6c41e90cc63c956408d57f851f7257009ba0d4
Haoze Xie <royenheart@gmail.com>
2024-08-12 19:04:32 +0800

    fix: Fix path not found error when list $PATH env

--- tinyget/common_utils.py
+++ tinyget/common_utils.py
@@ -24,8 +24,11 @@ def get_os_package_manager(possible_package_manager_names: List[str]):
     paths = os.environ["PATH"].split(os.pathsep)
     for bin_path in paths:
         for package_manager_name in possible_package_manager_names:
-            if package_manager_name in os.listdir(bin_path):
-                return package_manager_name
+            try:
+                if package_manager_name in os.listdir(bin_path):
+                    return package_manager_name
+            except FileNotFoundError as e:
+                continue
     raise Exception("No supported package manager found in PATH")
 
 
